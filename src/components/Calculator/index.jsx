361b8ba0379985769e165500d3a2f70d77423ee7
kevin <zhang.kevin.hua@gmail.com>
2022-11-15 01:46:57 -0600

    generally working

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,24 +1,81 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import './calculator.css'
 
-const Calculator = () => {
+// const operators = ['+', '-', '÷', '*', '(', ')'];
+// const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
+const Calculator = () => {
     const [input, setInput] = useState('');
     const [output, setOutput] = useState('');
+    // const [hasEvaluated, setHasEvaluated] = useState(false);
+    
+    const parseNum = str => str.replace(/\d*(\.\d+)?/g, n => n && +n);
+    const parseExpression = str => {
+        while (str.length > 1 && str[0] === '0') {
+            str = str.slice(1);
+        }
+        return parseNum(str)
+    }
 
+    const handleClick = e => {
+        const button = e.target.value;
+        switch (button) {
+            // When pressing equal button, try to evaluate the expression
+            case '=': {
+                let res;
+                let expression = input;
+                expression = parseExpression(expression);
+                try {
+                    res = new Function(`return ${expression}`)();
+                    res = eval(expression).toString();
+                } catch (error) { // If expression invalid, display an error
+                    res = 'Invalid Expression'
+                }
+                // Set display out to the evaluated result or return invalid
+                setOutput(res);
+                setInput(res);
+                break;
+            };
+            case '^': {
+                let res = input;
+                setInput(res += '**');
+                break;
+            }
+            case 'C': {
+                setInput("");
+                setOutput("");
+                break;
+            }
+            case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':case '0':case '(':case ')':case '*':case '+': case '-': case '÷':case '.': {
+                setOutput('');
+                let res = input;
+                setInput(res += button);
+                break;
+            }
+        }
+
+    }
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+
+    useEffect(() => {
+        console.log(input, output)
+    })
 
     const [row1, row2, row3, row4, row5] = [
-        ['C', '(', ')', '/'],
+        ['C', '(', ')', '÷'],
         ['7', '8', '9', '*'],
         ['4', '5', '6', '-'],
         ['1', '2', '3', '+'],
-        ['0', '.', '=']
+        ['0', '.', '^', '='],
     ];
     const buttons = {row1, row2, row3, row4, row5};
 
     for (let row in buttons) {
         buttons[row] = buttons[row].map(label =>
-            <button key={label} onClick={() => null} value={label} id={`id${label}`}>{label}</button>
+            <button key={label} onClick={handleClick} value={label} id={`id${label}`}>{label}</button>
         )
     }
 
@@ -26,11 +83,11 @@ const Calculator = () => {
         <>
             <div className="grid-container">
                 <div className="display">
-                    <div className="prev">
-
+                    <div className="input">
+                        <input value={input} type='text' onChange={handleChange}></input>
                     </div>
-                    <div className="curr">
-
+                    <div className="output">
+                        {output}
                     </div>
                 </div>
                 <div className="output">
