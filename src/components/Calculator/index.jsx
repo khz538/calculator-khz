fd60848ab0dd932d9e3967f7f91afe9d5d9a83e4
kevin <zhang.kevin.hua@gmail.com>
2022-11-14 18:06:26 -0600

    button styles sorted

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -3,10 +3,12 @@ import './calculator.css'
 
 const Calculator = () => {
 
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
 
 
     const [row1, row2, row3, row4, row5] = [
-        ['C', 'AC', '* -1', '/'],
+        ['C', '(', ')', '/'],
         ['7', '8', '9', '*'],
         ['4', '5', '6', '-'],
         ['1', '2', '3', '+'],
@@ -16,24 +18,31 @@ const Calculator = () => {
 
     for (let row in buttons) {
         buttons[row] = buttons[row].map(label =>
-            <button key={label} onClick={() => null} value={label} id={label}>{label}</button>
+            <button key={label} onClick={() => null} value={label} id={`id${label}`}>{label}</button>
         )
     }
 
     return (
         <>
-            <div className="input">
+            <div className="grid-container">
+                <div className="display">
+                    <div className="prev">
 
-            </div>
-            <div className="output">
+                    </div>
+                    <div className="curr">
 
-            </div>
-            <div className="buttons">
-                {buttons.row1}
-                {buttons.row2}
-                {buttons.row3}
-                {buttons.row4}
-                {buttons.row5}
+                    </div>
+                </div>
+                <div className="output">
+
+                </div>
+                <div className="buttons">
+                    {buttons.row1}
+                    {buttons.row2}
+                    {buttons.row3}
+                    {buttons.row4}
+                    {buttons.row5}
+                </div>
             </div>
         </>
     )
